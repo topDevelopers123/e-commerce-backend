03c1ee5d963846fdd514b2060b02c56fb76bc416
topDeveloper123 <jhahimanshu498@gmail.com>
2024-06-14 14:59:39 +0530

    some changes

--- src/validation/Validations.Schema.js
+++ src/validation/Validations.Schema.js
@@ -2,26 +2,36 @@ import vine from "@vinejs/vine";
 
 import { customError } from "./index.validation.js";
 
-vine.errorReporter = () => new customError()
+vine.errorReporter = () => new customError();
 
 export const UserSchema = vine.object({
-    name:vine.string().minLength(2).maxLength(30),
-    email:vine.string().email(),
-    phone:vine.string().minLength(10).maxLength(12),
-    password:vine.string().minLength(6).maxLength(16)
-})
-
+  name: vine.string().minLength(2).maxLength(30),
+  email: vine.string().email(),
+  phone: vine.string().minLength(10).maxLength(12),
+  password: vine.string().minLength(6).maxLength(16),
+});
 
 export const ProductSchema = vine.object({
-      title: vine.string().minLength(5).maxLength(25),
-      description: vine.string(50),
-      category: vine.string().maxLength(30),
-      sub_category: vine.string().maxLength(30),
-      sub_inner_category: vine.string().maxLength(30),
-      local_charges: vine.number(),
-      zonal_charges: vine.number(),
-      national_charges: vine.number(),
-      local_deadline:  vine.number(),
-      zonal_deadline: vine.number(),
-      national_deadline:  vine.number()
-})
+  title: vine.string().minLength(5).maxLength(25),
+  description: vine.string(50),
+  category: vine.string().maxLength(30),
+  sub_category: vine.string().maxLength(30),
+  sub_inner_category: vine.string().maxLength(30),
+  local_charges: vine.number(),
+  zonal_charges: vine.number(),
+  national_charges: vine.number(),
+  local_deadline: vine.number(),
+  zonal_deadline: vine.number(),
+  national_deadline: vine.number(),
+});
+
+export const SubInnerCategory = vine.object({
+  sub_inner_category_name: vine.string(),
+  parent_category1: vine.string(),
+  parent_category2: vine.string(),
+});
+
+export const SubCategory = vine.object({
+  sub_category_name: vine.string(),
+  parent_category: vine.string(),
+});
