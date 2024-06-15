18e924e16ab1ae1ae437493ef08ef0a9553530ac
topDevelopers123 <parasjisco@gmail.com>
2024-06-15 13:05:23 +0530

    product detail controller changes

--- src/controller/Product.controller.js
+++ src/controller/Product.controller.js
@@ -1,68 +1,72 @@
-import { ProductModel } from "../model/products.model.js"; 
+import { ProductModel } from "../model/products.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
+const CreateProduct = asyncHandler(async (req, res) => {
+  const data = req.body;
 
+  const Create = await ProductModel.create({ ...data, user_id: req.user?._id });
 
-const  CreateProduct = asyncHandler(async(req,res)=>{
-    const data = req.body
-    
-    const Create = await ProductModel.create({...data,user_id:req.user?._id})
+  return res.status(201).json({
+    message: "Product Created Successful",
+    data: Create,
+  });
+});
 
-    return res.status(201).json({
-        message:"Product Created Successful",
-        data:Create
-    })
-})
+const GetProduct = asyncHandler(async (_, res) => {
+  const data = await ProductModel.find({});
 
-const GetProduct = asyncHandler(async(_,res)=>{
-    const data = await ProductModel.find({})
+  return res.status(200).json({
+    message: "Data ",
+    data,
+  });
+});
 
-    return res.status(200).json({
-        message:"Data ",
-        data
-    })
-})
+const AdminGetProduct = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
 
-const AdminGetProduct = asyncHandler(async(req,res)=>{
-    const {_id} = req.user
+  const data = await ProductModel.find({ user_id: _id });
+  return res.status(200).json({
+    message: "data",
+    data,
+  });
+});
 
-    const data = await ProductModel.find({user_id:_id})
-    return res.status(200).json({
-        message:"data",
-        data
-    })
-})
+const DeleteProduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
 
-const DeleteProduct = asyncHandler(async(req,res)=>{
-    const {id} = req.params 
+  const find = await ProductModel.findById(id);
 
-    const find =  await ProductModel.findById(id)
+  if (!find) {
+    return res.status(403).json({ message: "data is not exist" });
+  }
 
-    if(!find){
-        return res.status(403).json({message:"data is not exist"})
-    }
-    
-    await ProductModel.findByIdAndDelete(id)
+  await ProductModel.findByIdAndDelete(id);
 
-    return res.status(200).json({
-        message:"Product delete Successful"
-    })
-})
+  return res.status(200).json({
+    message: "Product delete Successful",
+  });
+});
 
-const UpdateProduct = asyncHandler(async(req,res)=>{
-    const data = req.body
-    const {id} = req.params
+const UpdateProduct = asyncHandler(async (req, res) => {
+  const data = req.body;
+  const { id } = req.params;
 
-    const find = await ProductModel.findById(id)
-    if(!find){
-        return res.status(404).json({
-            message:"Product is not exist"
-        })
-    }
-    await ProductModel.findByIdAndUpdate(id,data)
-    return res.status(200).json({
-        message:"Product update Successful"
-    })
-})
+  const find = await ProductModel.findById(id);
+  if (!find) {
+    return res.status(404).json({
+      message: "Product is not exist",
+    });
+  }
+  await ProductModel.findByIdAndUpdate(id, data);
+  return res.status(200).json({
+    message: "Product update Successful",
+  });
+});
 
-export {CreateProduct,GetProduct,AdminGetProduct,DeleteProduct,UpdateProduct}
+export {
+  CreateProduct,
+  GetProduct,
+  AdminGetProduct,
+  DeleteProduct,
+  UpdateProduct,
+};
--- src/controller/productDetail.controller.js
+++ src/controller/productDetail.controller.js
@@ -1,62 +1,72 @@
+import mongoose from "mongoose";
 import { ProductDetailModel } from "../model/ProductDetail.model.js";
 import { ProductModel } from "../model/products.model.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
+const CreateProductDetails = asyncHandler(async (req, res) => {
+  const data = req.body;
+  const Create = await ProductDetailModel.create({
+    ...data,
+    user_id: req.user?._id,
+  });
 
-const  CreateProductDetails = asyncHandler(async(req,res)=>{
-    const data = req.body
-    
-    const Create = await ProductDetailModel.create({...data,user_id:req.user?._id})
+  await ProductModel.findByIdAndUpdate(data.product_id, {
+    $push: {
+      products_details: Create._id,
+    },
+  });
 
-    await ProductModel.findByIdAndUpdate(data.Product_id,{products_details:Create._id})
+  return res.status(201).json({
+    message: "Product Details Created Successful",
+    data: Create,
+  });
+});
 
-    return res.status(201).json({
-        message:"Product Details Created Successful",
-        data:Create
-    })
-})
+const AdminGetProductDetails = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
 
+  const data = await ProductDetailModel.find({ user_id: _id });
+  return res.status(200).json({
+    message: "data",
+    data,
+  });
+});
 
-const AdminGetProductDetails = asyncHandler(async(req,res)=>{
-    const {_id} = req.user
+const DeleteProductDetails = asyncHandler(async (req, res) => {
+  const { id } = req.params;
 
-    const data = await ProductDetailModel.find({user_id:_id})
-    return res.status(200).json({
-        message:"data",
-        data
-    })
-})
+  const find = await ProductDetailModel.findById(id);
 
-const DeleteProductDetails = asyncHandler(async(req,res)=>{
-    const {id} = req.params 
+  if (!find) {
+    return res.status(403).json({ message: "data is not exist" });
+  }
 
-    const find =  await ProductDetailModel.findById(id)
+  await ProductDetailModel.findByIdAndDelete(id);
 
-    if(!find){
-        return res.status(403).json({message:"data is not exist"})
-    }
-    
-    await ProductDetailModel.findByIdAndDelete(id)
+  return res.status(200).json({
+    message: "Product Details delete Successful",
+  });
+});
 
-    return res.status(200).json({
-        message:"Product Details delete Successful"
-    })
-})
+const UpdateProductDetails = asyncHandler(async (req, res) => {
+  const data = req.body;
+  const { id } = req.params;
 
-const UpdateProductDetails = asyncHandler(async(req,res)=>{
-    const data = req.body
-    const {id} = req.params
+  const find = await ProductDetailModel.findById(id);
+  if (!find) {
+    return res.status(404).json({
+      message: "Product details is not exist",
+    });
+  }
+  await ProductDetailModel.findByIdAndUpdate(id, data);
+  return res.status(200).json({
+    message: "Product Details update Successful",
+  });
+});
 
-    const find = await ProductDetailModel.findById(id)
-    if(!find){
-        return res.status(404).json({
-            message:"Product details is not exist"
-        })
-    }
-    await ProductDetailModel.findByIdAndUpdate(id,data)
-    return res.status(200).json({
-        message:"Product Details update Successful"
-    })
-})
-
-export {CreateProductDetails,AdminGetProductDetails,DeleteProductDetails,UpdateProductDetails}
+export {
+  CreateProductDetails,
+  AdminGetProductDetails,
+  DeleteProductDetails,
+  UpdateProductDetails,
+};
