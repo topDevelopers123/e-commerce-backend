d4f35c0bf00c6c3df224aa6d2d1ee4c6deddf14c
topDevelopers123 <parasjisco@gmail.com>
2024-06-17 16:40:14 +0530

    updated order controller

--- src/routes/Order.routes.js
+++ src/routes/Order.routes.js
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import { Authontication } from "../middleware/Auth.middleware.js";
 import { CreateOrder, UpdateOrder } from "../controller/order.controller.js";
+import { Ordervalidation } from "../helper/index.helper.js";
 
-const router = Router()
+const router = Router();
 
+router.route("/create").post(Authontication, Ordervalidation, CreateOrder);
+router.route("/update/:id").put(Authontication, UpdateOrder);
 
-router.route("/create").post(Authontication, CreateOrder)
-router.route("/update").post(Authontication, UpdateOrder)
-
-
-export default router
+export default router;
