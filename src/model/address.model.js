b2994cbc4d0617d92660a017af27bcfa0f9a197d
topDevelopers123 <parasjisco@gmail.com>
2024-07-04 11:03:59 +0530

    some changes in address in model

--- src/model/address.model.js
+++ src/model/address.model.js
@@ -7,6 +7,11 @@ const address = new Schema(
       trim: true,
       required: true,
     },
+    addressType: {
+      type: String,
+      trim: true,
+      required: true,
+    },
     phone: {
       type: String,
       trim: true,
