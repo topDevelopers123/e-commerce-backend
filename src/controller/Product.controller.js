8490e1ff39e67afacc6f25d3e4002f3475a700a8
topDeveloper123 <jhahimanshu498@gmail.com>
2024-07-22 18:16:28 +0530

    pagination

--- src/controller/Product.controller.js
+++ src/controller/Product.controller.js
@@ -143,8 +143,8 @@ const GetProduct = asyncHandler(async (req, res) => {
 const AdminGetProduct = asyncHandler(async (req, res) => {
   const { _id } = req.user;
   const { query } = req;
-  const page = query.page || 1;
-  const limit = query.limit || 1;
+  const page = Number(query.page )|| 1;
+  const limit = Number(query.limit) || 1;
   const newLimit = limit * (page - 1);
 
   const data = await ProductModel.aggregate([
